f399e21cc228112e515ecf6e27d87387e87ef51e
Jeddi212 <jedediahf51@gmail.com>
2022-07-20 16:24:05 +0700

    rfc: commented ws close in js 🌂

--- src/wesoc.cr
+++ src/wesoc.cr
@@ -1,6 +1,27 @@
-# TODO: Write documentation for `Wesoc`
-module Wesoc
-  VERSION = "0.1.0"
+require "http/server"
 
-  # TODO: Put your code here
+messages = [] of String
+SOCKET = [] of HTTP::WebSocket
+
+ws_handler = HTTP::WebSocketHandler.new do |socket|
+  puts "Socket opened"
+  SOCKET << socket
+  
+  socket.on_message do |message|
+    messages << message
+    SOCKET.each_with_index { |socket, i|
+      socket.send messages[i]
+    }
+  end
+  
+  socket.on_close do
+    SOCKET.delete(socket)
+    puts "Socket closed"
+  end
 end
+
+server  = HTTP::Server.new([ws_handler])
+address = server.bind_tcp "0.0.0.0", 3000
+puts "Listening on ws://#{address}"
+server.listen
+
